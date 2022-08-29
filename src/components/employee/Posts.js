270c46eeef6ebf40ccf47b580dbaa06183c3d706
jayantabasumatary1 <jayanta.basumatary@weareflexday.com>
2022-08-30 02:43:03 +0530

    done viewing available jobs

--- src/components/employee/Posts.js
+++ src/components/employee/Posts.js
@@ -1,10 +1,19 @@
-import { Card, CardContent, CardActions, Typography, Grid, Button} from '@mui/material'
-import React from 'react'
+import { Box, Card, CardContent, CardActions, Typography, Grid, Button} from '@mui/material'
+import React, { useState } from 'react'
+import { Dialog, DialogContent, IconButton, DialogTitle } from '@mui/material';
+import { Close as CloseIcon } from '@mui/icons-material';
 
 const Posts = ({post}) => {
+  const [open, setOpen] = useState(false)
+  const handleClickOpen=()=>{
+      setOpen(true);
+  };
+  const handleClose= ()=>{
+      setOpen(false);
+  };
 
   return (
-    
+<>
     <Grid item xs ={12} sm={6} md={4} >
             <Card sx={{
             border: "3px solid #0A1929",
@@ -33,12 +42,49 @@ const Posts = ({post}) => {
                 </Typography>
             </CardContent>
             <CardActions>
-        <Button size="small" variant='outlined'>view</Button>
+        <Button onClick={handleClickOpen} size="small" variant='outlined'>view</Button>
         <Button size="small" variant="outlined">apply</Button>
       </CardActions>
             </Card>
             </Grid>
             
+            <Dialog open={open}  onClose={(_, reason)=>{
+                        if (reason!== "backdropClick"){
+                            {handleClose()}
+                        }
+                    }} fullWidth
+                    
+                    PaperProps={{
+                        style: {
+                          backgroundColor: 'white',
+                          boxShadow: "none",  
+                        },
+                      }}>
+                        <DialogTitle><Box sx={{
+                        display: 'flex',
+                        alignItems: "center",
+                        justifyContent: "space-between",
+
+                    }}>{post.title}
+                        
+                        <IconButton  onClick={handleClose} >
+                            <CloseIcon />
+                        </IconButton>
+
+                    </Box></DialogTitle>
+                         <DialogContent sx={{
+                            padding: "1.5rem",
+                        }}>
+                          <Typography variant='body2' component="p"color="black"  >
+                          {post.body}
+                          </Typography>
+                          <Typography variant='body2' component="p"color="black"  >
+                          {post.id}
+                          </Typography>                      
+                       </DialogContent>
+                      </Dialog>
+                     
+                      </>
         )}   
 
 export default Posts
